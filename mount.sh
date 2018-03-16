9351d321c1fd48fe491f04b47e3342fea17267e0
Erik Nelson <erik@nsk.io>
2018-03-15 22:28:46 -0400

    Finally got it working

--- mount.sh
+++ mount.sh
@@ -7,13 +7,12 @@ BUNDLE_LIB_MOUNT_DEST=${_dir}/vendor/github.com/automationbroker/bundle-lib
 BROKER_CLIENT_GO_MOUNT_SRC=$GOPATH/src/github.com/automationbroker/broker-client-go
 BROKER_CLIENT_GO_MOUNT_DEST=${_dir}/vendor/github.com/automationbroker/broker-client-go
 
-umount -R $BUNDLE_LIB_MOUNT_DEST
-umount -R $BROKER_CLIENT_GO_MOUNT_DEST
+sudo umount $BUNDLE_LIB_MOUNT_DEST
+sudo umount $BROKER_CLIENT_GO_MOUNT_DEST
 
 mkdir -p $BUNDLE_LIB_MOUNT_DEST
 mkdir -p $BROKER_CLIENT_GO_MOUNT_DEST
 
-bindfs -u ernelson -g ernelson --no-allow-other \
-  $MOUNT_SRC $MOUNT_DEST
-bindfs -u ernelson -g ernelson --no-allow-other \
+bindfs -u ernelson -g ernelson $BUNDLE_LIB_MOUNT_SRC $BUNDLE_LIB_MOUNT_DEST
+bindfs -u ernelson -g ernelson \
   $BROKER_CLIENT_GO_MOUNT_SRC $BROKER_CLIENT_GO_MOUNT_DEST
