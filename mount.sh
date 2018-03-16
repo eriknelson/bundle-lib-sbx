11470073a4a63789cab60883b0dba74a0e051807
Erik Nelson <erik@nsk.io>
2018-03-15 22:08:22 -0400

    Adding in broker-client-go

--- mount.sh
+++ mount.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MOUNT_SRC=$GOPATH/src/github.com/automationbroker/bundle-lib
-MOUNT_DEST=${_dir}/vendor/github.com/automationbroker/bundle-lib
-mkdir -p $MOUNT_DEST
 
-echo "MOUNT_SRC: $MOUNT_SRC"
-echo "MOUNT_DEST: $MOUNT_DEST"
+BUNDLE_LIB_MOUNT_SRC=$GOPATH/src/github.com/automationbroker/bundle-lib
+BUNDLE_LIB_MOUNT_DEST=${_dir}/vendor/github.com/automationbroker/bundle-lib
+
+BROKER_CLIENT_GO_MOUNT_SRC=$GOPATH/src/github.com/automationbroker/broker-client-go
+BROKER_CLIENT_GO_MOUNT_DEST=${_dir}/vendor/github.com/automationbroker/broker-client-go
+
+umount -R $BUNDLE_LIB_MOUNT_DEST
+umount -R $BROKER_CLIENT_GO_MOUNT_DEST
+
+mkdir -p $BUNDLE_LIB_MOUNT_DEST
+mkdir -p $BROKER_CLIENT_GO_MOUNT_DEST
 
 bindfs -u ernelson -g ernelson --no-allow-other \
   $MOUNT_SRC $MOUNT_DEST
+bindfs -u ernelson -g ernelson --no-allow-other \
+  $BROKER_CLIENT_GO_MOUNT_SRC $BROKER_CLIENT_GO_MOUNT_DEST
